d64bb783fb225d0885c9db38cd11bf9c0f54e751
lukasso <lukasz.koladko@gmail.com>
2020-06-22 15:23:58 +0200

    seo optimalization: www redirect, sitemap, favicons, texts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,98 +1,107 @@
 module.exports = {
-  siteMetadata: {
-    title: `Trener Personalny Rafał Kiszło`,
-    description: `Trener personalny Mokotów. Twoja kondycja jest w najlepszych rękach.`,
-    siteUrl: `https://trenerrafal.pl`,
-  },
-  plugins: [
-  {
-  resolve: `gatsby-plugin-offline`,
-  options: {
-    precachePages: [`/`],
-  },
-},
-    
-     {
-      resolve: `gatsby-source-filesystem`,
+    siteMetadata: {
+        title: `Trener Personalny Rafał Kiszło`,
+        description: `Trener personalny Mokotów. Twoja kondycja jest w najlepszych rękach.`,
+        siteUrl: `https://trenerrafal.pl`,
+    },
+    plugins: [
+      {
+      resolve: 'gatsby-plugin-htaccess',
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
+        https: true,
+        www: false,
+        host: 'trenerrafal.pl', // if 'www' is set to 'false', be sure to also remove it here!
       },
     },
-
-    `gatsby-plugin-react-helmet`,
-    {
-    resolve: `gatsby-plugin-sharp`,
-    options: {
-      useMozJpeg: false,
-      stripMetadata: true,
-      defaultQuality: 75,
-    },
-  },
-    {
-      resolve: `gatsby-transformer-sharp`,
-      options: {
-        // The option defaults to true
-        checkSupportedExtensions: false,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-         disableAutoprefixing: false,
-        disableMinification: false,
-      },
-    },    
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-offline`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 925,
-              // Remove the default behavior of adding a link to each
-              // image.
-              linkImagesToOriginal: true,
-              // Analyze images' pixel density to make decisions about
-              // target image size. This is what GitHub is doing when
-              // embedding images in tickets. This is a useful setting
-              // for documentation pages with a lot of screenshots.
-              // It can have unintended side effects on high pixel
-              // density artworks.
-              //
-              // Example: A screenshot made on a retina screen with a
-              // resolution of 144 (e.g. Macbook) and a width of 100px,
-              // will be rendered at 50px.
-              //
-              // Defaults to false.
-              sizeByPixelDensity: false,
+                precachePages: [`/`],
             },
-          },
-        ],
-      },
-    },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: ``,
-    //   },
-    // },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Rafał Kiszło - trener personalny Mokotów',
-        short_name: 'Rafał Kiszło',
-        start_url: '/',
-        icon: "src/pages/images/logo.png",
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        display: "standalone",
-        crossOrigin: `use-credentials`,
-      },
-    },
-  ],
-}
+        },
+
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        },
+
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                useMozJpeg: false,
+                stripMetadata: true,
+                defaultQuality: 75,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-sharp`,
+            options: {
+                // The option defaults to true
+                checkSupportedExtensions: false,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-material-ui`,
+            options: {
+                disableAutoprefixing: false,
+                disableMinification: false,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 925,
+                            // Remove the default behavior of adding a link to each
+                            // image.
+                            linkImagesToOriginal: true,
+                            // Analyze images' pixel density to make decisions about
+                            // target image size. This is what GitHub is doing when
+                            // embedding images in tickets. This is a useful setting
+                            // for documentation pages with a lot of screenshots.
+                            // It can have unintended side effects on high pixel
+                            // density artworks.
+                            //
+                            // Example: A screenshot made on a retina screen with a
+                            // resolution of 144 (e.g. Macbook) and a width of 100px,
+                            // will be rendered at 50px.
+                            //
+                            // Defaults to false.
+                            sizeByPixelDensity: false,
+                        },
+                    },
+                ],
+            },
+        },
+        // {
+        //   resolve: `gatsby-plugin-google-analytics`,
+        //   options: {
+        //     trackingId: ``,
+        //   },
+        // },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'Rafał Kiszło - trener personalny Mokotów',
+                short_name: 'Rafał Kiszło',
+                start_url: '/',
+                icon: 'src/pages/images/logo.png',
+                background_color: '#ffffff',
+                theme_color: '#000000',
+                display: 'standalone',
+                crossOrigin: `use-credentials`,
+            },
+        },
+    ],
+};
--- src/components/testimondials/dataTestimondials.js
+++ src/components/testimondials/dataTestimondials.js
@@ -4,7 +4,7 @@ export const dataTestimondials = [
     id: 4,
     text:
       'Rafal has really helped me to get back to the gym, stay consistent with my trainings, and take care of my body. Rafal understands my physical strengths and weaknesses and tailors my workouts to challenge me and avoid injuring areas where I am building strength. The workouts are both rigorous, but also enjoyable for me. I look for to meeting up with Rafal for my trainings which has kept me coming back to the gym more consistently than ever. I highly recommend Rafal to anyone who is seeking a knowledgeable and attentive personal trainer.',
-    name: 'Imię',
+    name: 'Dave',
     position: 'Zawód',
     picture: './images/4.jpg',
     date: 'styczeń, 2020',
@@ -13,7 +13,7 @@ export const dataTestimondials = [
     id: 5,
     text:
       'Rafal to profesjonalista. Z tym trenerem nie trzeba się obawiać monotonnych treningów – są urozmaicone, ciekawe i skuteczne. Atmosfera treningu jest przyjacielska i sympatyczna ale nie lekka. Daje wycisk :)',
-    name: 'Imię',
+    name: 'Dominik',
     position: 'Zawód',
     picture: './images/5.jpg',
     date: 'styczeń, 2020',
@@ -22,7 +22,7 @@ export const dataTestimondials = [
     id: 6,
     text:
       'Trenujemy z Rafałem od września 2016 roku. Moc, energia, forma, rzeźba. Polecam ale trzeba się przygotować na prawdziwy trening 😄💪🏼💪🏼💪🏼',
-    name: 'Imię',
+    name: 'Piotr',
     position: 'Zawód',
     picture: './images/6.jpg',
     date: 'styczeń, 2020',
@@ -31,7 +31,7 @@ export const dataTestimondials = [
     id: 7,
     text:
       'Rafał dobiera ćwiczenia do trybu życia i możliwości ćwiczącego. Ćwiczenia są bardzo różnorodne, a do tego Rafał jest sympatycznym i ciekawym gościem więc nie można się nudzić :) Potrafi też zmotywować żeby na treningu dać z siebie wszystko. Zdecydowanie polecam treningi z Rafałem!',
-    name: 'Imię',
+    name: 'Mateusz',
     position: 'Zawód',
     picture: './images/7.jpg',
     date: 'styczeń, 2020',
@@ -40,7 +40,7 @@ export const dataTestimondials = [
     id: 8,
     text:
       'Treningi z Rafałem to ciągłe podnoszenie poprzeczki, nowe wyzwania i mnóstwo radości! Rafał  jest perfekcyjne przygotowany merytoryczne, śledzi nowe trendy w fitnessie i ma dużą wiedzę o odżywianiu. Pilnuje poprawności wykonywanych ćwiczeń, doradza przy planowaniu posiłków, ale przede wszystkim niesamowicie motywuje do dawania z siebie jak najwięcej. Treningi z Rafałem polecam każdemu!',
-    name: 'Imię',
+    name: 'Ania',
     position: 'Zawód',
     picture: './images/8.jpg',
     date: 'styczeń, 2020',
@@ -49,7 +49,7 @@ export const dataTestimondials = [
     id: 9,
     text:
       'Ćwiczę już z Rafałem od ponad 1 roku i jestem bardzo zadowolony z efektów. Rafał jest zawsze bardzo profesjonalny, słowny i co najważniejsze - posiada bardzo dużą wiedzę z tematów związanych ze sportem i zdrowiem. Każdy trening odbywa się w miłej atmosferze z dużą dawką humoru i śmiechu, co mnie dodatkowo motywuje i sprawiło, że nigdy nie opuściłem żadnego treningu z Rafałem. Szczerze polecam!',
-    name: 'Imię',
+    name: 'Michał',
     position: 'Zawód',
     picture: './images/9.jpg',
     date: 'styczeń, 2020',
@@ -58,7 +58,7 @@ export const dataTestimondials = [
     id: 10,
     text:
       'Rafał to profesjonalny trener - nie ma dla niego rzeczy niemożliwych do osiągnięcia.',
-    name: 'Imię',
+    name: 'Mariusz',
     position: 'Zawód',
     picture: './images/10.jpg',
     date: 'styczeń, 2020',
@@ -67,7 +67,7 @@ export const dataTestimondials = [
     id: 11,
     text:
       'Zdecydowanie polecam Rafała, jest przede wszystkim specjalista z ukończonymi studiami, i stałe rozwijającym się w swoje branży. Holistyczne podejście i nie tylko wieków ciężary ale i wiele różnorakich ćwiczeń. Mega wiedza i świetny kontakt, zdecydowanie polecam !!!!!',
-    name: 'Imię',
+    name: 'Łukasz',
     position: 'Zawód',
     picture: './images/11.jpg',
     date: 'styczeń, 2020',
@@ -76,7 +76,7 @@ export const dataTestimondials = [
     id: 12,
     text:
       'Trening z Rafałem zaczęliśmy od wywiadu “środowiskowego”, rozmowa była nakierowana na poznanie mojej przeszłości w kontekście aktywności fizycznej, stylowi życia oraz ewentualnych urazów. Następnie przeszliśmy do zarysowania celi i sprawdzeniu obecnych możliwości, wszystko przebiegało w bardzo swobodnej atmosferze. Jako początkującemu (zasiedziały biurowy 30latek) bardzo zależało mi aby trener zwracał uwagę na poprawność postawy oraz wykonywanych ćwiczeń, co robi na każdych zajęciach i cały czas stara się przyglądać i korygować niepoprawnie wykonywane powtórzenia. Dodatkowo, do tej pory praktycznie żadne zajęcia nie miały tych samych ćwiczeń, oprócz standardowych sztang, maszyn, etc. korzystamy z worków bułgarskich, różnych piłek, gum, pełna różnorodność uruchamiająca wszystkie partie mięśni, co bardzo mi odpowiada. Jestem na początku swojej drogi, ale z pewnością mogę Rafała polecić!',
-    name: 'Imię',
+    name: 'Jan',
     position: 'Zawód',
     picture: './images/12.jpg',
     date: 'styczeń, 2020',
@@ -85,7 +85,7 @@ export const dataTestimondials = [
     id: 13,
     text:
       'Poszukiwania idealnego trenera nie były łatwe miałam jednak wielkie szczęście spotkać Rafała. Dla mnie przede wszystkim najważniejszy był powrót do poprawnej postawy ciała i zakończeniu coraz częstszych problemów z plecami czy  obręczą barkową. Od momentu rozpoczęcia treningu dolegliwości zdecydowanie się zmniejszyły. Jest to mój początek treningów ale wreszcie widzę możliwość wyjścia z problemów bólowych. Wszystkie ćwiczenia ułożone są  do indywidualnych potrzeb, Rafał objaśnia dokładnie na czym polega ćwiczenia i jaki ma cel, dodatkowo pomaga w ułożeniu ćwiczeń które można swobodnie powtórzyć w domu- podsumowując serdeczny, pomocny trener z ogromna wiedzą w obszarze treningu holistycznego.',
-    name: 'Imię',
+    name: 'Ania',
     position: 'Zawód',
     picture: './images/13.jpg',
     date: 'styczeń, 2020',
